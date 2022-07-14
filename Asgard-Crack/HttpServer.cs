e6af946514ce9fe1b501d6282c063d6c983bc93b
ニャー猫 <69488850+0ez@users.noreply.github.com>
2022-07-14 08:10:59 -0700

    Add files via upload

---
+++ Asgard-Crack/HttpServer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+//
+//  Yes, I know that doing shit like that (2 diff servers) is dumb, but the user will only use them for few seconds
+//
+
+namespace HttpServer
+{
+    public class Server1
+    {
+        public static int Port = 80;
+
+        private static HttpListener _listener;
+
+        public static void Start()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://*:" + Port.ToString() + "/api/v1/user/");
+            _listener.Start();
+            Receive();
+        }
+
+        public static void Stop()
+        {
+            _listener.Stop();
+        }
+
+        private static void Receive()
+        {
+            _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+        }
+        private static void ProcessRequest(HttpListenerContext context)
+        {
+            // Get the data from the HTTP stream
+            var body = new StreamReader(context.Request.InputStream).ReadToEnd();
+
+            string content = "{'message': 'success', 'account': {'username': 'cracked by parkie#3604', 'tokenAccessToken': 'fuckpython', 'banned': 'false', 'expired': 'false', 'expirationDate': '2077-01-01', 'daysLeft': 69420, 'preset': 'null'}}";
+            string download = "{'download':'https://cdn.discordapp.com/attachments/804178032280600647/986163950079053874/A-R.exe','version':'R1.1.0'}";
+
+            byte[] b = Encoding.UTF8.GetBytes(content);
+
+            
+            context.Response.StatusCode = 200;
+            context.Response.KeepAlive = false;
+            context.Response.ContentLength64 = b.Length;
+
+            var output = context.Response.OutputStream;
+            output.Write(b, 0, b.Length);
+            context.Response.Close();
+        }
+        private static void ListenerCallback(IAsyncResult result)
+        {
+
+            if (_listener.IsListening)
+            {
+                var context = _listener.EndGetContext(result);
+                var request = context.Request;
+
+                ProcessRequest(context);
+
+                Receive();
+            }
+        }
+    }
+
+    public class Server2
+    {
+        public static int Port = 80;
+
+        private static HttpListener _listener;
+
+        public static void Start()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://*:" + Port.ToString() + "/api/v1/application/");
+            _listener.Start();
+            Receive();
+        }
+
+        public static void Stop()
+        {
+            _listener.Stop();
+        }
+
+        private static void Receive()
+        {
+            _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+        }
+        private static void ProcessRequest(HttpListenerContext context)
+        {
+            // Get the data from the HTTP stream
+            var body = new StreamReader(context.Request.InputStream).ReadToEnd();
+
+            string download = "{'download':'https://cdn.discordapp.com/attachments/804178032280600647/986163950079053874/A-R.exe','version':'R1.1.0'}";
+
+            byte[] b = Encoding.UTF8.GetBytes(download);
+
+
+            context.Response.StatusCode = 200;
+            context.Response.KeepAlive = false;
+            context.Response.ContentLength64 = b.Length;
+
+            var output = context.Response.OutputStream;
+            output.Write(b, 0, b.Length);
+            context.Response.Close();
+        }
+        private static void ListenerCallback(IAsyncResult result)
+        {
+
+            if (_listener.IsListening)
+            {
+                var context = _listener.EndGetContext(result);
+                var request = context.Request;
+
+                ProcessRequest(context);
+
+                Receive();
+            }
+        }
+    }
+}
---
+++ Asgard-Crack/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+using HttpServer;
+
+namespace Asgard_Crack
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "asgard crack - made by parkie#3604";
+            Console.WindowWidth = 180;
+            Console.WindowHeight = 40;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("\n\n");
+            //             ....................................................................................................................................................................................
+            Console.Write("                                                                                     ##                                                      /        \n");
+            Console.Write("                                                                                     ##                                                    #/         \n");
+            Console.Write("                                                                                     ##                                                    ##         \n");
+            Console.Write("                                                                                     ##                                                    ##         \n");
+            Console.Write("                                                                                     ##                                                    ##         \n");
+            Console.Write("                              /###      /###      /###      /###   ###  /###     ### ##              /###   ###  /###     /###     /###    ##  /##    \n");
+            Console.Write("                             / ###  /  / #### /  /  ###  / / ###  / ###/ #### / #########           / ###  / ###/ #### / / ###  / / ###  / ## / ###   \n");
+            Console.Write("                            /   ###/  ##  ###/  /    ###/ /   ###/   ##   ###/ ##   ####           /   ###/   ##   ###/ /   ###/ /   ###/  ##/   /    \n");
+            Console.Write("                           ##    ##  ####      ##     ## ##    ##    ##        ##    ##           ##          ##       ##    ## ##         ##   /     \n");
+            Console.Write("                           ##    ##    ###     ##     ## ##    ##    ##        ##    ##           ##          ##       ##    ## ##         ##  /      \n");
+            Console.Write("                           ##    ##      ###   ##     ## ##    ##    ##        ##    ##           ##          ##       ##    ## ##         ## ##      \n");
+            Console.Write("                           ##    ##        ### ##     ## ##    ##    ##        ##    ##           ##          ##       ##    ## ##         ######     \n");
+            Console.Write("                           ##    /#   /###  ## ##     ## ##    /#    ##        ##    /#           ###     /   ##       ##    /# ###     /  ##  ###    \n");
+            Console.Write("                            ####/ ## / #### /   ########  ####/ ##   ###        ####/              ######/    ###       ####/ ## ######/   ##   ### / \n");
+            Console.Write("                             ###   ##   ###/      ### ###  ###   ##   ###        ###                #####      ###       ###   ## #####     ##   ##/  \n");
+            Console.Write("                                                       ###                                        \n");
+            Console.Write("                                                 ####   ###                                       \n");
+            Console.Write("                                               /######  /#                                        \n");
+            Console.Write("                                              /     ###/                                          \n\n\n\n\n\n");
+
+
+
+            Console.Write("                                                                            [+] made by parkie#3604\n\n");
+            Console.Write("                                                                            [+] github.com/0ez\n\n"); Thread.Sleep(500);
+            // 127.0.0.1 a086e0efbad65f0bb.awsglobalaccelerator.com
+            if (!File.ReadAllText("C:\\Windows\\System32\\drivers\\etc\\hosts").Contains("127.0.0.1 a086e0efbad65f0bb.awsglobalaccelerator.com"))
+            {
+                using (FileStream fs = new FileStream("C:\\Windows\\System32\\drivers\\etc\\hosts", FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    StreamWriter write = new StreamWriter(fs);
+                    write.BaseStream.Seek(0, SeekOrigin.End);
+                    write.WriteLine(Environment.NewLine);
+                    write.WriteLine("127.0.0.1 a086e0efbad65f0bb.awsglobalaccelerator.com");
+                    write.Flush();
+                    write.Close();
+                    fs.Close();
+                }
+            }
+            //File.Delete("C:\\Windows\\System32\\drivers\\etc\\hosts");
+            //WebClient webClient = new WebClient();
+            //webClient.DownloadFile("https://cdn.discordapp.com/attachments/724153110808887340/996905777577087056/hosts", "C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+            Server1.Start();
+            Server2.Start();
+
+            Console.Write("                                                                            [+] bypass running!\n\n");
+            Console.Write("                                                                            [+] run a-r.exe\n\n");
+
+            Console.ReadKey();
+        }
+    }
+}
